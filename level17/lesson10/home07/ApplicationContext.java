1546747cf7b9b75945a0e0fd73d48390408dbbd0
splDead <splD@yandex.ru>
2016-07-19 10:53:57 +0300

    Add files via upload

---
+++ level17/lesson10/home07/ApplicationContext.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level17.lesson10.home07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* ApplicationContext
+ApplicationContext будет доступен множеству нитей.
+Сделать так, чтобы данные не терялись: подумай, какое ключевое слово необходимо поставить и где.
+*/
+
+public abstract class ApplicationContext<GenericsBean extends Bean> {
+    private Map<String, GenericsBean> container = new HashMap<String, GenericsBean>();
+    //Map<Name, some class implemented Bean interface>
+
+
+    protected ApplicationContext() {
+        parseAllClassesAndInterfaces();
+    }
+
+    /*
+    Синхронизируем геттер и сеттер, чтобы не сработали одновременно.
+    Для того, чтобы сеттер не удалил объект, пока его читает геттер.
+     */
+
+    synchronized public GenericsBean getByName(String name) {
+          return container.get(name);
+    }
+
+    synchronized public GenericsBean removeByName(String name) {
+        return container.remove(name);
+    }
+
+    protected abstract void parseAllClassesAndInterfaces();
+}
