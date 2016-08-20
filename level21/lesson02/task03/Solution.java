f99b4981cfa5acfa049f6d176fabf5e7e1259c67
splDead <splD@yandex.ru>
2016-08-20 15:55:27 +0300

    Add files via upload

---
+++ level21/lesson02/task03/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level21.lesson02.task03;
+
+/* Все гениальное - просто!
+Упростить. Переменные не переименовывать, комментарии не оставлять.
+*/
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c;
+    }
+}
+
+/**
+ http://info.javarush.ru/uploads/images/00/09/32/2014/06/17/1e9088.png
+ очень помогло это объяснение
+ */
