0b66cee8127a8c5be7808c8284c045d4c8ba8339
splDead <splD@yandex.ru>
2016-09-20 20:31:08 +0300

    Add files via upload

---
+++ level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by splD on 20.09.2016.
+ */
+public interface SelfInterfaceMarker
+{
+}
---
+++ level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by splD on 20.09.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+    public SelfInterfaceMarkerImpl()
+    {
+    }
+
+    public void PrintA() {
+        System.out.println("A");
+    }
+
+    public void PrintB() {
+        System.out.println("B");
+    }
+
+    public void PrintC() {
+        System.out.println("C");
+    }
+}
